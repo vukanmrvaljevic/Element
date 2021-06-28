539a37df72628666c9e49b17e16b91bde2049457
vukanmrvaljevic <vukanmrvaljevic@gmail.com>
2021-06-28 14:35:53 -0400

    deploying

--- src/App.js
+++ src/App.js
@@ -24,14 +24,14 @@ class App extends React.Component {
     const api_call = await fetch(
       `https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`
     )
-    const data = await api_call.json()
+    const weatherData = await api_call.json()
     if (city && country) {
       this.setState({
-        temperature: data.main.temp,
-        city: data.name,
-        country: data.sys.country,
-        humidity: data.main.humidity,
-        description: data.weather[0].description,
+        temperature: weatherData.main.temp,
+        city: weatherData.name,
+        country: weatherData.sys.country,
+        humidity: weatherData.main.humidity,
+        description: weatherData.weather[0].description,
         error: "",
       })
     } else {
